15c389b829afeb425d135d33e1132faa49d8f72e
jpe17 <joaopaesteves99@gmail.com>
2024-12-17 17:38:38 +0000

    Connection to DB successfull

--- backend/database.sql
+++ backend/database.sql
@@ -1,11 +1,25 @@
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(255),
-  city VARCHAR(100),
-  price DECIMAL
+  name VARCHAR(255),
+  host_id INT,
+  host_name VARCHAR(255),
+  neighbourhood_group VARCHAR(100),
+  neighbourhood VARCHAR(100),
+  latitude DECIMAL,
+  longitude DECIMAL,
+  room_type VARCHAR(50),
+  price DECIMAL,
+  minimum_nights INT,
+  number_of_reviews INT,
+  last_review DATE,
+  reviews_per_month DECIMAL,
+  calculated_host_listings_count INT,
+  availability_365 INT,
+  number_of_reviews_ltm INT,
+  license VARCHAR(50)
 );
 
-INSERT INTO properties (title, city, price) VALUES
-('Luxury Apartment', 'London', 500000),
-('Cozy Cottage', 'Edinburgh', 250000),
-('Modern Flat', 'Manchester', 300000);
+COPY properties(name, host_id, host_name, neighbourhood_group, neighbourhood, latitude, longitude, room_type, price, minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, availability_365, number_of_reviews_ltm, license)
+FROM '/docker-entrypoint-initdb.d/data/airbnb_listings.csv' 
+DELIMITER ',' 
+CSV HEADER;
